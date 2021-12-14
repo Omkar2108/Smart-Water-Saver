af5806388ed471e0ada3022186900f5cee19bf9f
omkar shevare <omkarshevare7@gmail.com>
2021-12-14 12:25:15 +0530

    Login, SignUp page done | Omkar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,104 @@
-import { View , Text, Button } from 'react-native';
+import { View , Text, Button, ImageBackground, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
-
+import FadeInView from '../components/fadeInView';
 function HomeScreen({ navigation }) {
     return (
-        <View style={{padding:10, margin:10}}>
-            <Text> This is HomeScreen !</Text>
-            <Button  title="Login" onPress={()=>  navigation.navigate("Login")}></Button>
-            <Button  title="Sign Up"  onPress={()=>  navigation.navigate("SignUp")}></Button>
+        <View style={styles.container}>
+            <ImageBackground
+            source={require("../assets/4.jpg")}
+            resizeMode="cover"
+            style={styles.image}
+            >
+
+             <FadeInView>
+                <View style={styles.container}>
+                    <View style={styles.textBox}>
+                    <Text style={styles.font}>
+                        You are one step away from saving water !
+                    </Text>
+                    </View>
+
+                    <View style={styles.fixToText}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => navigation.navigate("Login")}
+                    >
+                        <Text style={styles.buttonText}>L O G I N</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => navigation.navigate("SignUp")}
+                    >
+                        <Text style={styles.buttonText}>S I G N U P</Text>
+                    </TouchableOpacity>
+                    </View>
+                </View>
+                </FadeInView>
+            {/* <Button  title="Login" onPress={()=>  navigation.navigate("Login")}></Button>
+            <Button  title="Sign Up"  onPress={()=>  navigation.navigate("SignUp")}></Button> */}
+            </ImageBackground>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      height: "100%",
+      width: "100%",
+      justifyContent: "space-between"
+    },
+    image: {
+      width: "100%",
+      height:"100%"
+    },
+    textBox: {
+      justifyContent: "center",
+      textAlign: "center",
+      marginTop: 52,
+      marginLeft: "10%",
+      height: "auto",
+      width: "80%",
+      padding: 12,
+      borderRadius: 12
+    },
+    font: {
+      textShadowColor: "rgba(0, 0, 0, 0.5)",
+      textShadowOffset: { width: -5, height: 5 },
+      textShadowRadius: 6,
+      fontWeight: "bold",
+      fontSize: 32,
+      color: "#010014",
+      alignItems:'center',
+      justifyContent:'center',
+    //   flex:'col'
+    },
+    fixToText: {
+      flexDirection: "column",
+      justifyContent: "space-evenly",
+      marginBottom: "5%"
+    },
+  
+    button: {
+      shadowColor: "#7fffd4",
+      shadowOffset: { width: -4, height: 4 },
+      shadowOpacity: 0.29,
+      shadowRadius: 9,
+      marginLeft: "7%",
+      marginBottom: "5%",
+      height: 49,
+      width: "86%",
+      backgroundColor: "#000000",
+      borderColor: "#7fffd4",
+    //   borderWidth: ,
+      padding: 13,
+      borderRadius: 12
+    },
+    buttonText: {
+      color: "#20b2aa",
+      fontSize: 15,
+      alignSelf: "center"
+    }
+  });
+
 
 export default HomeScreen;
--- screens/Login.js
+++ screens/Login.js
@@ -1,12 +1,183 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { useNavigation } from "@react-navigation/native";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image
+} from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import React from "react";
+
 
 function Login() {
-    return (
-        <View>
-            <Text> This is Login Page!</Text>
+  const navigation = useNavigation();
+
+  return (
+    <KeyboardAwareScrollView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.backgrad}></View>
+        <View style={styles.box}>
+          <View style={styles.grad}>
+            <View style={styles.imageBox}>
+              <Image
+                style={styles.images}
+                source={require("../assets/login-1.png")}
+              />
+            </View>
+            <Text style={styles.Headerfont}>Welcome Back</Text>
+          </View>
+          <View style={styles.flexing}>
+            <Text style={styles.font}>Please Enter Username and Password</Text>
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder="username"
+                placeholderTextColor="black"
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="password"
+                placeholderTextColor="black"
+              />
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate("LoginPage")}
+              >
+                <Text style={styles.buttonText}>L O G I N</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-    )
+      </View>
+    </KeyboardAwareScrollView>
+  );
 }
 
-export default Login
+const styles = StyleSheet.create({
+  grad: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6
+    },
+    shadowOpacity: 0.26,
+    shadowRadius: 8,
+    paddingTop: 10,
+    paddingBottom: 10,
+    justifyContent: "space-between",
+    height: 180,
+    backgroundColor: "#1f7882",
+    borderRadius: 10
+  },
+  backgrad: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6
+    },
+    shadowOpacity: 0.26,
+    shadowRadius: 11,
+    position: "absolute",
+    elevation: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    justifyContent: "space-between",
+    width: "100%",
+    height: 265,
+    backgroundColor: "#1f7882"
+  },
+  input: {
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.29,
+    shadowRadius: 9,
+    height: 40,
+    marginBottom: 15,
+    marginLeft: 16,
+    marginRight: 16,
+    borderWidth: 1,
+    paddingLeft: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    borderRadius: 10,
+    color: "black"
+  },
+
+  images: {
+    marginTop: 20,
+    height: 70,
+    width: 70,
+    alignSelf: "center"
+  },
+  box: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6
+    },
+    shadowOpacity: 0.56,
+    shadowRadius: 13,
+    height: 480,
+    textAlign: "center",
+    marginTop: 100,
+    marginLeft: 40,
+    marginRight: 40,
+    borderRadius: 20,
+    backgroundColor: "#F9F7F7"
+  },
+  flexing: {
+    height: 320,
+    justifyContent: "space-evenly"
+  },
+  container: {
+    backgroundColor: "#F9F7F7",
+    height: "100%",
+    width: "100%"
+  },
+  Headerfont: {
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    alignSelf: "center",
+    textShadowColor: "rgba(0, 0, 0, 0.6)",
+    textShadowOffset: { width: 0, height: 3 },
+    textShadowRadius: 6,
+    elevation: 25,
+    fontSize: 30,
+    color: "#ffffff"
+  },
+  font: {
+    alignSelf: "center",
+    textShadowColor: "rgba(0, 0, 0, 0.100000000)",
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 6,
+    fontSize: 20,
+    color: "#1f7882",
+    marginLeft: 15,
+    marginRight: 15
+  },
+  button: {
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.29,
+    shadowRadius: 9,
+    marginLeft: 45,
+    marginRight: 45,
+    marginTop: 5,
+    elevation: 25,
+    height: 40,
+    backgroundColor: "#1f7882",
+    borderColor: "#7fffd4",
+    // padding: 12,
+    borderRadius: 16
+  },
+  buttonText: {
+    color: "#ffff",
+    fontSize: 15,
+    marginTop: 11
+  }
+});
+
+export default Login;
